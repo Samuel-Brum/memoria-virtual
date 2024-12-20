2415f9865d3be50a3efc327352adf930debd85fd
BuxGuerra <61712585+BuxGuerra@users.noreply.github.com>
2024-12-20 11:49:33 -0300

    Arquivo com algs para trocar pags

---
+++ trocaPag.c
@@ -0,0 +1,28 @@
+void trocarPagDensa(unsigned long num_pag, int num_pag_troca, EntradaTabelaPagina* tabela_paginas, Quadro* quadros) {
+    int num_quadro_troca = tabela_paginas[num_pag_troca].numero_quadro;
+
+    if(quadros[num_quadro_troca].modificado == 1) {
+        tabela_paginas[num_pag_troca].modificado = 1;
+    }
+
+    tabela_paginas[num_pag_troca].numero_quadro = -1;
+    tabela_paginas[num_pag_troca].valido = 0;
+
+    tabela_paginas[num_pag].numero_quadro = num_quadro_troca;
+    tabela_paginas[num_pag].valido = 1;
+
+    quadros[num_quadro_troca].numero_pagina = num_pag;
+    quadros[num_quadro_troca].modificado = 0;
+}
+
+void trocarPagHierarquica2() {
+    
+}
+
+void trocarPagHierarquica3() {
+    
+}
+
+void trocarPagInvertida() {
+
+}
